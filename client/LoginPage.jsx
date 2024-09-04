96c7e30ecd67bd23580c521f84772fa2693b1cee
Rakeshkumarsahugithub <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2024-09-04 23:32:45 +0530

    Update LoginPage.jsx

--- client/LoginPage.jsx
+++ client/LoginPage.jsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
             return;
         }
 
-        axios.post(`${window.location.origin}/login`, { email, password })
+        axios.post('https://mern-api-lyart.vercel.app/login', { email, password })
             .then(res => {
                 if (res.data.Login) { // Use correct key from response
                     navigate('/home');
