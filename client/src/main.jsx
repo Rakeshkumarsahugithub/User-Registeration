ead676dd6580b80b81ab8757629b582109f2bc1e
Rakeshkumarsahugithub <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2024-09-09 17:40:07 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import HomePage from './HomePage.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <HomePage/>
   </StrictMode>,
 )
